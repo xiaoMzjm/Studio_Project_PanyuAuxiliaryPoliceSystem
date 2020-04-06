0b3a6b99947a3f6fef0a2e441bf62ab7c1aa038e
zhangjiaming <383248689@qq.com>
2020-04-06 15:11:31 +0800

    枚举接口完成

---
+++ houtai/base/biz/biz-user/biz-user-client/src/main/java/com/base/biz/user/client/model/EnumVO.java
@@ -0,0 +1,32 @@
+package com.base.biz.user.client.model;
+
+/**
+ * @author:小M
+ * @date:2020/4/6 1:38 PM
+ */
+public class EnumVO {
+
+    private Integer code;
+    private String name;
+
+    public EnumVO(Integer code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ houtai/base/biz/biz-user/biz-user-server/src/main/java/com/base/biz/user/server/api/BizUserEnumController.java
@@ -0,0 +1,142 @@
+package com.base.biz.user.server.api;
+
+import com.alibaba.fastjson.JSON;
+
+import com.base.biz.user.client.common.Enums.AuthorizedStrengthTypeEnum;
+import com.base.biz.user.client.common.Enums.DimssionTypeEnum;
+import com.base.biz.user.client.common.Enums.DrivingTypeEnum;
+import com.base.biz.user.client.common.Enums.EducationEnum;
+import com.base.biz.user.client.common.Enums.EnrollWayEnum;
+import com.base.biz.user.client.common.Enums.ExservicemanEnum;
+import com.base.biz.user.client.common.Enums.JobCategoryEnum;
+import com.base.biz.user.client.common.Enums.JopGradeEnum;
+import com.base.biz.user.client.common.Enums.MaritalStatusEnum;
+import com.base.biz.user.client.common.Enums.NationEnum;
+import com.base.biz.user.client.common.Enums.PersonnelTypeEnum;
+import com.base.biz.user.client.common.Enums.PlaceOfWorkEnum;
+import com.base.biz.user.client.common.Enums.PoliticalLandscapeEnum;
+import com.base.biz.user.client.common.Enums.SexEnum;
+import com.base.biz.user.client.common.Enums.TreatmentGradeEnum;
+import com.base.common.annotation.ResultFilter;
+import com.base.common.constant.Result;
+import com.base.user.client.model.TokenFilter;
+import io.swagger.annotations.Api;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author:小M
+ * @date:2020/4/6 1:20 PM
+ */
+@Api("人员管理枚举")
+@Controller
+@ResponseBody
+@RequestMapping(value = "/user/enum/" , produces = "application/json;charset=UTF-8")
+public class BizUserEnumController {
+
+    @ResultFilter
+    @TokenFilter
+    @RequestMapping("nation")
+    public String nation(){
+        return JSON.toJSONString(Result.success(NationEnum.getAll()));
+    }
+
+    @ResultFilter
+    @TokenFilter
+    @RequestMapping("politicallandscape")
+    public String politicallandscape(){
+        return JSON.toJSONString(Result.success(PoliticalLandscapeEnum.getAll()));
+    }
+
+    @ResultFilter
+    @TokenFilter
+    @RequestMapping("drivingtype")
+    public String drivingtype(){
+        return JSON.toJSONString(Result.success(DrivingTypeEnum.getAll()));
+    }
+
+    @ResultFilter
+    @TokenFilter
+    @RequestMapping("exserviceman")
+    public String exserviceman(){
+        return JSON.toJSONString(Result.success(ExservicemanEnum.getAll()));
+    }
+
+    @ResultFilter
+    @TokenFilter
+    @RequestMapping("sex")
+    public String sex(){
+        return JSON.toJSONString(Result.success(SexEnum.getAll()));
+    }
+
+    @ResultFilter
+    @TokenFilter
+    @RequestMapping("education")
+    public String education(){
+        return JSON.toJSONString(Result.success(EducationEnum.getAll()));
+    }
+
+    @ResultFilter
+    @TokenFilter
+    @RequestMapping("maritalstatus")
+    public String maritalstatus(){
+        return JSON.toJSONString(Result.success(MaritalStatusEnum.getAll()));
+    }
+
+    @ResultFilter
+    @TokenFilter
+    @RequestMapping("personneltype")
+    public String personneltype(){
+        return JSON.toJSONString(Result.success(PersonnelTypeEnum.getAll()));
+    }
+
+    @ResultFilter
+    @TokenFilter
+    @RequestMapping("authorizedstrengthype")
+    public String authorizedstrengthype(){
+        return JSON.toJSONString(Result.success(AuthorizedStrengthTypeEnum.getAll()));
+    }
+
+    @ResultFilter
+    @TokenFilter
+    @RequestMapping("placeofwork")
+    public String placeofwork(){
+        return JSON.toJSONString(Result.success(PlaceOfWorkEnum.getAll()));
+    }
+
+    @ResultFilter
+    @TokenFilter
+    @RequestMapping("jobgrade")
+    public String jobgrade(){
+        return JSON.toJSONString(Result.success(JopGradeEnum.getAll()));
+    }
+
+    @ResultFilter
+    @TokenFilter
+    @RequestMapping("treatmentgrade")
+    public String treatmentgrade(){
+        return JSON.toJSONString(Result.success(TreatmentGradeEnum.getAll()));
+    }
+
+    @ResultFilter
+    @TokenFilter
+    @RequestMapping("enrollway")
+    public String enrollway(){
+        return JSON.toJSONString(Result.success(EnrollWayEnum.getAll()));
+    }
+
+    @ResultFilter
+    @TokenFilter
+    @RequestMapping("dimssiontype")
+    public String dimssiontype(){
+        return JSON.toJSONString(Result.success(DimssionTypeEnum.getAll()));
+    }
+
+    @ResultFilter
+    @TokenFilter
+    @RequestMapping("jobcategory")
+    public String jobcategory(){
+        return JSON.toJSONString(Result.success(JobCategoryEnum.getAll()));
+    }
+}
